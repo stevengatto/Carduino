31efaa6970ddff7b2e206ddec3f58d64bc33645f
Anthony Reid <AnthonyReid99@gmail.com>
2014-02-09 01:31:04 -0500

    Preferences implemented, app uses saved ip and port, places user in settings if settigns not set

--- src/com/fsc/Carduino/ControllerFragment.java
+++ src/com/fsc/Carduino/ControllerFragment.java
@@ -1,7 +1,11 @@
 package com.fsc.Carduino;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -14,14 +18,31 @@ import com.fsc.Datagram.UDPMailMan;
  */
 public class ControllerFragment extends Fragment {
 
+    public static final String PREF_KEY_IP_ADDRESS = "pref_key_host_ip";
+    public static final String PREF_KEY_HOST_PORT = "pref_key_host_port";
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        //Ensure that the settings contain a port and ip address
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+        String ipAddress = prefs.getString(PREF_KEY_IP_ADDRESS, "");
+        String port = prefs.getString(PREF_KEY_HOST_PORT, "");
+        if (ipAddress.equals("") || port.equals("")) {
+            Intent intent = new Intent(activity, PreferencesActivity.class);
+            startActivity(intent);
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 
-        // Set the ip and port of the arduino
-        UDPMailMan.setPort(2390);
-        UDPMailMan.setIpAddress("192.168.0.107");
-
+        // Grab the IP and port from preferences
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        UDPMailMan.setPort(Integer.parseInt(prefs.getString(PREF_KEY_HOST_PORT, "2390")));
+        UDPMailMan.setIpAddress(prefs.getString(PREF_KEY_IP_ADDRESS, "127.0.0.0"));
 
         View viewTreeRoot = inflater.inflate(R.layout.fragment_controller, container, false);
 
